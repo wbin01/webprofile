57bdcaf7b2e18883e4bc4b7a4c7def8222cf1d0b
ali55on <wildenalisson@outlook.com>
2022-09-12 15:55:18 -0300

    pages

--- webprofile/urls.py
+++ webprofile/urls.py
@@ -4,4 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('content', content, name='content'),
+    path('create', create, name='create'),
+    path('login', login, name='login'),
+    path('logout', logout, name='logout'),
+    path('signup', signup, name='signup'),
 ]
--- webprofile/views.py
+++ webprofile/views.py
@@ -4,3 +4,28 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+
+def content(request):
+    context = {}
+    return render(request, 'content.html', context)
+
+
+def create(request):
+    context = {}
+    return render(request, 'create.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    context = {}
+    return render(request, 'logout.html', context)
+
+
+def signup(request):
+    context = {}
+    return render(request, 'signup.html', context)
