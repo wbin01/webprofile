c4505c81d44f7445e1acf624ae38d58e1e3768c8
ali55on <wildenalisson@outlook.com>
2022-09-12 11:43:44 -0300

    env

--- setup/urls.py
+++ setup/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from dotenv import load_dotenv
+
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+load_dotenv()
+ADMIN_ROUTE = os.getenv('ADMIN_ROUTE')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path(ADMIN_ROUTE, admin.site.urls),
     path('', include('webprofile.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
