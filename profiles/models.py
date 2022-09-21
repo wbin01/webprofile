cbd280d7a5e42b9522977fbe5f366750550dadeb
ali55on <wildenalisson@outlook.com>
2022-09-21 10:47:50 -0300

    Profile Model

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
         size=[100, 100], crop=['middle', 'center'],
         upload_to='profile_image/%d/%m/%Y/', blank=True, null=True)
     cover_image = ResizedImageField(
-        size=[100, 100], crop=['middle', 'center'],
+        size=[300, 1000], crop=['middle', 'center'],
         upload_to='profile_cover_image/%d/%m/%Y/', blank=True, null=True)
 
     def __str__(self):
