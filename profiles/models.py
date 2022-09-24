6643f4d41d553a1e3bbb719feef871497caf487f
ali55on <wildenalisson@outlook.com>
2022-09-24 00:41:05 -0300

    Update profile layout

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from django_resized import ResizedImageField
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile_image = ResizedImageField(
-        size=[100, 100], crop=['middle', 'center'],
+        size=[150, 150], crop=['middle', 'center'],
         upload_to='profile_image/%d/%m/%Y/', blank=True, null=True)
     cover_image = ResizedImageField(
         size=[1500, 300], crop=['middle', 'center'],
