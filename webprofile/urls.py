1f8a32e2069e939bd91696ae10f6aac15bcfc2fc
ali55on <wildenalisson@outlook.com>
2022-09-27 10:13:51 -0300

    'create' and 'edit' size

--- webprofile/urls.py
+++ webprofile/urls.py
@@ -4,9 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('post:<str:url_title>-<int:post_id>', content, name='content'),
+    path('post/<str:url_title>:<int:post_id>', content, name='content'),
     path('create/sender:<str:url_to_go_back>', create, name='create'),
-    path('edit/<str:url_title>-<int:post_id>/sender:<str:url_to_go_back>', edit, name='edit'),
+    path('edit/<str:url_title>:<int:post_id>/sender:<str:url_to_go_back>',
+         edit, name='edit'),
     path('update/<int:post_id>', update, name='update'),
-    path('delete/<str:url_to_go_back>-<int:post_id>', delete, name='delete'),
+    path('delete/<str:url_to_go_back>:<int:post_id>', delete, name='delete'),
 ]
