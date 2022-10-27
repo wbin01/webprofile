712785f15cc8648bbb151e8e42f9e938a2c3f423
ali55on <wildenalisson@outlook.com>
2022-10-27 16:48:36 -0300

    requirements

--- setup/urls.py
+++ setup/urls.py
@@ -31,5 +31,4 @@ urlpatterns = [
     path('', include('webprofile.urls')),
     path('', include('users.urls')),
     path('profiles/', include('profiles.urls')),
-    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
